9ffa424dc1d0e8b4933d209a39ab069150ce2ad7
sumitkumarsharm <sumitkumarsharma2748@gmail.com>
2025-12-21 23:29:42 +0530

    created Dashboard char bar

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,21 @@
+export const chartData = [
+  {
+    nmae: "High",
+    total: 2400,
+  },
+  {
+    nmae: "Medium",
+    total: 2210,
+  },
+  {
+    nmae: "Low",
+    total: 2290,
+  },
+];
+
+export const summary = {
+  totalTask: 10,
+  last10Task: [],
+  users: [],
+  tasks: [],
+};
---
+++ frontend/src/components/Chart.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+// Pass 'data' as a prop
+const Chart = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} width={150} height={40}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Bar dataKey="total" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default Chart;
